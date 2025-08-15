b8b0a769a648ed0c1fcac81e6bf58b13be333c6b
Kizzuato <kyyajuara@gmail.com>
2025-08-15 14:43:33 +0700

    fixing get all data path & fixing auto sync

--- index.js
+++ index.js
@@ -1,51 +1,57 @@
 // app.js
-const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { sequelize, User } = require('./models');
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { sequelize, User } = require("./models");
+require("dotenv").config();
 
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/', async (req, res) => {
-  res.status(201).json({ message: 'Halo dari API smart-train'})
-})
+app.get("/", async (req, res) => {
+  res.status(201).json({ message: "Halo dari API smart-train" });
+});
 
 // Register
-app.post('/register', async (req, res) => {
+app.post("/register", async (req, res) => {
   const { username, password } = req.body;
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({ username, password: hashedPassword });
-    res.status(201).json({ message: 'User registered successfully' });
+    console.log("JWT_SECRET in login:", process.env.JWT_SECRET);
+    console.log("JWT_SECRET in me:", process.env.JWT_SECRET);
+    res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
-    res.status(500).json({ message: 'Error registering user', error });
+    res.status(500).json({ message: "Error registering user", error });
   }
 });
 
 // Login
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   const { username, password } = req.body;
   try {
     const user = await User.findOne({ where: { username } });
     if (!user || !(await bcrypt.compare(password, user.password))) {
-      return res.status(401).json({ message: 'Invalid username or password' });
+      return res.status(401).json({ message: "Invalid username or password" });
     }
-    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
     res.json({ token });
   } catch (error) {
-    res.status(500).json({ message: 'Error logging in', error });
+    res.status(500).json({ message: "Error logging in", error });
   }
 });
 
 // Me
 app.get('/me', async (req, res) => {
-  const token = req.headers['authorization'];
-  if (!token) {
+  const authHeader = req.headers['authorization'];
+  if (!authHeader) {
     return res.status(401).json({ message: 'No token provided' });
   }
+
+  const token = authHeader.split(' ')[1]; // ambil setelah "Bearer"
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findByPk(decoded.userId);
@@ -59,8 +65,8 @@ app.get('/me', async (req, res) => {
 });
 
 // Sync database and start server
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync().then(() => {
   app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+    console.log("Server is running on port 5000");
   });
 });

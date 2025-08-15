a7b5b047efa9410c3b018a23fb60b6d05f7b9a85
Kizzuato <kyyajuara@gmail.com>
2025-08-15 13:59:44 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+// app.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { sequelize, User } = require('./models');
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', async (req, res) => {
+  res.status(201).json({ message: 'Halo dari API smart-train'})
+})
+
+// Register
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ username, password: hashedPassword });
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error registering user', error });
+  }
+});
+
+// Login
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ where: { username } });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ message: 'Invalid username or password' });
+    }
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ message: 'Error logging in', error });
+  }
+});
+
+// Me
+app.get('/me', async (req, res) => {
+  const token = req.headers['authorization'];
+  if (!token) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findByPk(decoded.userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.json({ user });
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching user', error });
+  }
+});
+
+// Sync database and start server
+sequelize.sync({ force: true }).then(() => {
+  app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+  });
+});
